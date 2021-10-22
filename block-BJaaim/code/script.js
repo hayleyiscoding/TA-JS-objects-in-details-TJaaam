f7ab56e8a27bfa31bf0a0901e7d98fccc2ccb150
Hayley Wood <onlinecowork@gmail.com>
2021-10-23 03:35:19 +1000

    oop assignment 3 block 3 update

--- block-BJaaim/code/script.js
+++ block-BJaaim/code/script.js
@@ -1,57 +1,69 @@
-let allQuestions = [
+let quizCollection = [
   {
     title: "What is the capital of Spain?",
     options: ["Madrid", "Barcelona", "Seville"],
-    correctAnswer: 0,
+    correctAnswerIndex: 0,
   },
   {
     title: "What is the capital of England?",
     options: ["Perth", "London", "Dublin"],
-    correctAnswer: 1,
+    correctAnswerIndex: 1,
   },
   {
     title: "What is the capital of South Korea?",
     options: ["Pyongyang", "Seoul", "Busan"],
-    correctAnswer: 1,
+    correctAnswerIndex: 1,
   },
   {
     title: "What is the capital of Japan?",
     options: ["Seoul", "Tokyo", "Osaka"],
-    correctAnswer: 1,
+    correctAnswerIndex: 1,
   },
   {
     title: "What is the capital of Thailand?",
     options: ["Phuket", "Chiang Mai", "Bangkok"],
-    correctAnswer: 2,
+    correctAnswerIndex: 2,
   },
 ];
 
 let form = document.querySelector("form");
 let root = document.querySelector("ul");
+let button = document.querySelector("button");
 
 class Question {
-  constructor(title, options, correctAnswer) {
+  constructor(title, options, correctAnswerIndex) {
     this.title = title;
     this.options = options;
-    this.correctAnswerIndex = correctAnswer;
+    this.correctAnswerIndex = correctAnswerIndex;
   }
   isCorrect(answer) {
-    return answer === this.correctAnswer;
+    return answer === this.options[this.correctAnswerIndex];
+  }
+  getCorrectAnswer() {
+    return this.options[this.correctAnswerIndex];
   }
 }
 
-let 
 class Quiz {
-  constructor(allQuestions) {
-    this.allQuestions = allQuestions;
+  constructor(questions = [], score = 0) {
+    this.questions = questions;
+    this.score = score;
     this.activeIndex = 0;
   }
+  addQuestion(title, options, correctAnswerIndex) {
+    let question = new Question(title, options, correctAnswerIndex);
+    this.questions.push(question);
+  }
+  prevQuestion() {
+    this.activeIndex = this.activeIndex - 1;
+    this.createUI();
+  }
   nextQuestion(index) {
-    let question = new Question(title, options, correctAnswer);
-    this.allQuestions[index + 1];
+    this.activeIndex = this.activeIndex + 1;
     this.createUI();
   }
   createUI() {
+    root.innerHTML = "";
     //      <li>
     //     <p>What's the capital of Spain?</p>
     //     <input type="radio" class="option1" name="question" value="option1" />
@@ -62,38 +74,53 @@ class Quiz {
     //     <label for="option3">Seville</label>
     //     <button>Next Question >>></button>
     //   </li>
-    let questions = this.allQuestions[this.activeIndex];
+    let activeQuestion = this.questions[this.activeIndex];
+    console.log(activeQuestion);
     let li = document.createElement("li");
     let p = document.createElement("p");
+    p.innerText = activeQuestion.title;
     let input1 = document.createElement("input");
+    input1.type = "radio";
     input1.classList.add("option1");
     input1.value = "option1";
     input1.name = "question";
     let label1 = document.createElement("label");
     label1.for = "option1";
-    label1.innerText = this.options[0];
+    label1.innerText = activeQuestion.options[0];
     let input2 = document.createElement("input");
+    input2.type = "radio";
     input2.classList.add("option2");
     input2.value = "option2";
     input2.name = "question";
     let label2 = document.createElement("label");
     label2.for = "option2";
-    label2.innerText = this.options[1];
+    label2.innerText = activeQuestion.options[1];
     let input3 = document.createElement("input");
     input3.classList.add("option3");
     input3.value = "option3";
     input3.name = "question";
+    input3.type = "radio";
     let label3 = document.createElement("label");
     label3.for = "option1";
-    label3.innerText = this.options[2];
+    label3.innerText = activeQuestion.options[2];
     let button = document.createElement("button");
     button.innerText = "Next Question >>>";
-    let button = document.querySelector("button");
-    button.addEventListener("click", () => {
-      this.question.activeIndex = this.question.activeIndex + 1;
-      this.createUI();
-    });
+    button.addEventListener("click", quiz.nextQuestion.bind(quiz));
     li.append(p, input1, label1, input2, label2, input3, label3, button);
     root.append(li);
   }
 }
+
+let quiz = new Quiz();
+// quiz.createUI();
+quizCollection.forEach((question) => {
+  quiz.addQuestion(
+    question.title,
+    question.options,
+    question.correctAnswerIndex
+  );
+});
+quiz.createUI();
+
+// button.addEventListener("click", quiz.nextQuestion.bind(quiz));
+// prev.addEventListener("click", quiz.prevQuestion.bind(quiz));
